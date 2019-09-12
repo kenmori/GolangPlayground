362838698ced3a276109f1fb2c92de2eddb406f5
kenjimorita <>
2019-09-12 19:52:07 +0900

    init

---
+++ lesson.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+// func init (){
+//     fmt.Println("init!")
+// }
+func bazzz() {
+    fmt.Printf("fafa")
+}
+func main() {
+    // bazzz();
+    fmt.Printf("Hello world\n")
+}
