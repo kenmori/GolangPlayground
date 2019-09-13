3351df441e4e32175eb2e054d6aae7b58720f2c1
kenjimorita <>
2019-09-13 19:52:02 +0900

    add strings

--- lesson.go
+++ lesson.go
@@ -2,19 +2,20 @@ package main
 
 import (
     "fmt"
+    "strings"
 )
 func main() {
-    // var (
-    //     u8  uint8     = 255
-    //     i8  int8      = 127
-    //     f32 float32   = 0.2
-    //     c64 complex64 = -5 + 12i
-    // )
-    // fmt.Println(u8, i8, f32, c64)
-    // fmt.Printf("type=%T value=%v", u8, u8)
-    fmt.Println("1 + 1=", 1+1)
-    fmt.Println("10 + 1=", 10+1)
-    fmt.Println("10 / 3=", 10/3)
-    fmt.Println("10 / 2=", 10/2)
-    fmt.Println("10 / 3.0=", 10.0/3)
+    fmt.Println("Hello World")
+    fmt.Println("Hello" + "World")
+    fmt.Println(string("HelloWorld"[0]))
+    var s string = "Hello World"
+    s = strings.Replace(s, "H", "W", 1) // 最初の1つめ
+    fmt.Println(s)
+    fmt.Println(`
+        Test
+                Test
+    Test
+    `)
+    fmt.Println("\"")
+    fmt.Println(`"`)
 }
