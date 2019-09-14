d8404a25ffc9cfbeb271aefe4738785074185eb4
kenjimorita <>
2019-09-14 20:01:39 +0900

    quetion

--- lesson.go
+++ lesson.go
@@ -4,18 +4,29 @@ import (
 	"fmt"
 )
 
-func thirdPartyConnectDB() {
-	panic("Unable to connect database!") // panicはなるべく使わないことがGolangでは推奨されている errorhandlingするべき
-}
-func save() {
-	defer func() { // saveが終わったら実行sれる。panicで強制終了されるのでそのrecoverをしてる
-		s := recover()
-		fmt.Println(s)
-	}()
-	thirdPartyConnectDB() // deferより上に書くと強制終了してrecoverまで到達しない
-}
-
 func main() {
-	save()
-	fmt.Println("OK?")
+	// 一番少ない値を出力するコード
+	l := []int{100, 300, 23, 11, 23, 2, 4, 6, 4}
+	var min [1]int
+	for i, v := range l {
+		fmt.Println(i, v)
+		if min[0] > v {
+			min[0] = v
+		}
+	}
+	fmt.Println("min is", min[0])
+	// 合計を出力するコード
+	m := map[string]int{
+		"apple":  200,
+		"banana": 300,
+		"grapes": 150,
+		"orange": 80,
+		"papaya": 500,
+		"kiwi":   90,
+	}
+	var amont int
+	for _, v := range m {
+		amont = +v
+	}
+	fmt.Println(amont)
 }
