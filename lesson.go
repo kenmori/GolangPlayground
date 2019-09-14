500500dd6363c4ba8fc394f47ab656a6b7ede580
kenjimorita <>
2019-09-14 10:54:22 +0900

    slice

--- lesson.go
+++ lesson.go
@@ -5,7 +5,19 @@ import (
 )
 
 func main() {
-	var b = []int{100}
-	b = append(b, 300)
-	fmt.Println(b)
+	var n = []int{1,2,3,4,5,6,7,8}
+	fmt.Println(n)
+	fmt.Println(n[2:4])
+	fmt.Println(n[:2])
+	fmt.Println(n[2:])
+	fmt.Println(n[:])
+	n[2] = 100
+	var board = [][]int{
+		[]int{0, 1, 2},
+		[]int{3, 4, 5},
+	}
+	fmt.Println(board[0][1])
+	n = append(n, 100)
+	fmt.Println(n)
+
 }
