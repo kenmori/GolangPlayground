b80b2cb0227a0b178b8805c9de3a066170216722
kenjimorita <>
2019-09-15 15:10:16 +0900

    add22

--- lesson.go
+++ lesson.go
@@ -4,32 +4,31 @@ import (
 	"fmt"
 )
 
-type Human interface {
-	Say() string
-}
-type Person struct {
-	Name string
-}
+func do (i interface{}){ // どんな型でも受け取るので // interfaceのものの型を変更するのがタイプアサーション
+	// ss := i.(string)
+	// ii := i.(int) // ここでタイプアサーションしなくてはいけない
+	// ii *= 2
+	// fmt.Println(ii)
+	// fmt.Println(ss + "!")
+	switch v := i.(type) {
+		case int:
+			fmt.Printf("%T %d\n", v, v)
+		case string:
+			fmt.Printf("%T %s\n", v, v)
+		default:
+			fmt.Printf("i don know")
 
-type Dog struct {
-	Name string
-}
-
-func (p *Person) Say() string { // 実装した中身を操作するのでアスタリスク
-	p.Name = "Mr." + p.Name
-	return p.Name
-}
-func DriveCar (human Human) {
-	if human.Say() == "Mr.Mike" {
-		fmt.Println("ok")
-	} else {
-		fmt.Println("no")
 	}
+	var a int = 10
+	aa := float64(10) // タイプコンバージョン。interfaceでないのでタイプアサーションではない
+	fmt.Println(a, aa)
+
 }
 func main() {
-	var mike Human = &Person{"Mike"}
-	var dog Dog = Dog{"wanwan"}
-	DriveCar(mike)
-	DriveCar(dog)
+	// var i interface{} = 10 //do(10)と同じ意味。10を代入したからといってiはintegerではないので。そのご受け取り側でType Assertionする必要がある
+	// do(i)
+	do(10)
+	do("Mike")
+	do(true)	
 }
 
