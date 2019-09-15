055dfea797153cbf3a9b3191522a7ca3800dc4ce
kenjimorita <>
2019-09-15 14:00:16 +0900

    interface

--- lesson.go
+++ lesson.go
@@ -4,32 +4,20 @@ import (
 	"fmt"
 )
 
-type Vertex struct{
-	x, y int
+type Human interface {
+	Say()
 }
-type Vertex3D struct {
-	Vertex
-	z int
+type Person struct {
+	Name string
 }
 
-func (v Vertex3D) Area3D () int { // 値レシーバー
-	return v.x * v.y * v.z
-}
-func (v *Vertex3D) Scale3D (i int) { // ポイントレシーバー。Vertexの値を書き換えられる
-	v.x = v.x * i
-	v.y = v.y * i
-	v.z = v.z * i
-}
-func New (x, y, z int) *Vertex3D {
-	fmt.Println(Vertex3D{Vertex{x, y}, z})
-	return &Vertex3D{Vertex{x, y}, z}
+func (p *Person) Say() {
+	p.Name = "Mr." + p.Name
+	fmt.Println(p.Name)
 }
 
 func main() {
-	// v := Vertex{3, 4}
-	// fmt.Println(v.Area()) // .でvのメソッドになる
-	v := New(3, 4, 5)
-	v.Scale3D(10)
-	fmt.Println(v.Area3D())
+	var mike Human = &Person{"Mike"}
+	mike.Say()
 }
 
