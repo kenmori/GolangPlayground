048b37f69321ea4c0b8df05e3e0d4d5151908dc7
kenjimorita <>
2019-09-21 08:47:42 +0900

    POST

--- main.go
+++ main.go
@@ -12,11 +12,12 @@ func main() {
 	// defer resp.Body.Close()
 	// body, _ := ioutil.ReadAll(resp.Body)
 	// fmt.Println(string(body)
-	base, _ := url.Parse("http://example.com") // check invalid url
-	reference, _ := url.Parse("/test?a=1&b=2")
+	base, _ := url.Parse("http://example.com")            // check invalid url // POSTの場合https://が多い
+	reference, _ := url.Parse("/test?a=1&b=2")            // Getの時はこのように情報をわたし、
 	endpoint := base.ResolveReference(reference).String() // base urlにたいしてreferenceをつける
 	fmt.Println(endpoint)
 	req, _ := http.NewRequest("GET", endpoint, nil) // structをつくっているだけ
+	// req, _ := http.NewREquest("POST", endpoint, bytes.NewBuffer([]byte("password"))) // POSTの場合、bufferで領域を確保して情報を渡す。// bytes.NewBuffer([]byte("password")はbody
 	req.Header.Add("If-None-Match", `W/"wyzzy"`)
 	q := req.URL.Query()
 	q.Add("c", "3&%")
